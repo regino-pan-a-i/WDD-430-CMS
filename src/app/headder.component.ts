a24b7e2fd79a839e81609f9809f7695ce3f453c3
Andre Regino <reginopanai@gmail.com>
2025-05-02 15:45:17 -0600

    initial commit, created basic components

---
+++ src/app/headder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-headder',
+  standalone: false,
+  templateUrl: './headder.component.html',
+  styleUrl: './headder.component.css'
+})
+export class HeadderComponent {
+
+}
