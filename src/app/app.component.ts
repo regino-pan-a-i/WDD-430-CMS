4f87cf0db75d414448031fde9fc7b4ba937d20af
Andre Regino <reginopanai@gmail.com>
2025-05-11 00:48:22 -0600

    Implemented view switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cms';
+  selectedFeature = "Documents"
+  
+  switchView(selectedFeature: string){
+    this.selectedFeature = selectedFeature
+  }
+
 }
