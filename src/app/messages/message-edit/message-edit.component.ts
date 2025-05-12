001a636692a21a1bb9e83b507ec65e4e00b661bf
Andre Regino <reginopanai@gmail.com>
2025-05-11 23:16:16 -0600

    Assignment 3 completed

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.component.css'
 })
 export class MessageEditComponent {
+
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  private idCounter = 0;
+
+  addMessageSubmit(message: Event) {
+    message.preventDefault();
+  
+    const form = message.target as HTMLFormElement;
+    const subject = form['subject'].value;
+    const messageText = form['message'].value;
+  
+    console.log('Subject:', subject);
+    console.log('Message:', messageText);
+  
+    this.idCounter += 1;
+  
+    const incomingMessage = new Message(this.idCounter, subject, messageText, 'Lali Cuadora');
+    this.addMessageEvent.emit(incomingMessage);
+  }
 
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -8,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class MessageListComponent {
 
+  messages: Array<Message> = [];
+
+  message1 = new Message(1, 'Late for class', 'I apologize I could not get to class on time', 'Lali Cuadora');
+  message2 = new Message(2, 'Late for class', 'No worries, life happens', 'Bro. Hilton');
+  message3 = new Message(3, 'Late for class', 'Is there any way I can make up for the absence?', 'Lali Cuadora');
+  message4 = new Message(4, 'Late for class', 'No', 'Bro. Hilton');
+
+  constructor() {
+    this.messages.push(this.message1);
+    this.messages.push(this.message2);
+    this.messages.push(this.message3);
+    this.messages.push(this.message4);
+  }
+
+
+  onAddMessage(message: Message){
+    this.messages.push(message)
+  }
 }
